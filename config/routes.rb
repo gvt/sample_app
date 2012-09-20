7f75ecbf0f5eb8174bbe1f40d3bf5d68e26202bd
Greg Tomei <gregtomei@gmail.com>
2012-09-20 11:03:16 -0700

    adding UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  get "user/new"
+
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+
   get "static_pages/home"
-
   get "static_pages/help"
-
   get "static_pages/about"
 
   # The priority is based upon order of creation:
